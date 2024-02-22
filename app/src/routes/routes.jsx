fc541e4434664fa2f25c959080fb0deca3416f3e
Jonathan Luembe <jonathanluembe@yahoo.com>
2024-02-22 11:37:57 +0100

    img

--- app/src/routes/routes.jsx
+++ app/src/routes/routes.jsx
@@ -1,11 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
-
 import ErrorPage from "./ErrorPage/ErrorPage";
-
 import QuizzPageHtml from "./QuizzPageHtml/QuizzPageHtml";
 import QuizzPageJs from "./QuizzPageJs/QuizzPageJs";
-import QuizzPageCss from "./QuizzPageCss/QuizzPageCss";
 
 export const routesConfig = [
   {
@@ -22,11 +19,12 @@ export const routesConfig = [
     path: "/quizz-js",
     element: <QuizzPageJs />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "/quizz-css",
-    element: <QuizzPageCss />,
+  }, 
+   {
+    path: "/*",
+    element: <ErrorPage />,
     errorElement: <ErrorPage />,
   },
+
 ];
 export const router = createBrowserRouter(routesConfig);
