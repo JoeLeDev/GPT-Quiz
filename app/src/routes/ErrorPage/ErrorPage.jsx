25e9f51502c6bbdd76950d7a0040a3cdb0f9e931
lsun <lsun.github@gmail.com>
2024-02-22 11:42:25 +0100

    Delete image1.png and update ErrorPage.jsxi

--- app/src/routes/ErrorPage/ErrorPage.jsx
+++ app/src/routes/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom"
 import "./ErrorPage.css"
+import image from "../../assets/image1.png"
 
 const ErrorPage = () => {
   return (
     <div className='container'>
       <h1>Pourquoi tu quittes la page</h1>
-      <img src="../../../public/assets/image1.png" alt="" />
+      <img src={image} alt="" />
       <Link to="/">Retour</Link>
     </div>
   );
