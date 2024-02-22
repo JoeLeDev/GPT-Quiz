0b2c7f814767cf92a39fdf6b38e1d365161708ec
lsun <lsun.github@gmail.com>
2024-02-22 16:55:48 +0100

    Add Button component to HomePage

---
+++ app/src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./button.css";
+
+export default function Button({ label, variant, handleClick }) {
+  return (
+    <button className={`button ${variant}`} onClick={handleClick}>
+      {label}
+    </button>
+  );
+}
