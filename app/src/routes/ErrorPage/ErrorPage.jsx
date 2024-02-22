8a031d8c23fb24df7834c408850bb98ddf5d9f8b
Jonathan Luembe <jonathanluembe@yahoo.com>
2024-02-22 11:24:04 +0100

    add ini main.css

--- app/src/routes/ErrorPage/ErrorPage.jsx
+++ app/src/routes/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+<<<<<<< Updated upstream
 
 const ErrorPage = () => {
   return (
     <div>
       <h1>Error Page</h1>
       <p>Oops! Something went wrong.</p>
+=======
+import { Link } from "react-router-dom"
+import "./ErrorPage.css"
+const ErrorPage = () => {
+  return (
+    <div className='container'>
+      <h1>Pourquoi tu quittes la page</h1>
+      <Link to="/">Retour</Link>
+>>>>>>> Stashed changes
     </div>
   );
 };
