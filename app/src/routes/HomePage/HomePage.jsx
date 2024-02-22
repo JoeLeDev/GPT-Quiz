a84be90150c663d1505139d51e99834b6cd6f6bd
lsun <lsun.github@gmail.com>
2024-02-22 21:57:02 +0100

    Refactor Button component and add navigation functionality to HomePage

--- app/src/routes/HomePage/HomePage.jsx
+++ app/src/routes/HomePage/HomePage.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import { React } from "react";
 import Button from "../../components/Button/Button";
 import Title from "../../components/Title/Title";
+import { useNavigate } from "react-router-dom";
 import "./homePage.css";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="container-home">
       <Title text={"GPT QUIZ"} />
       <div className="button-container">
-        <Button variant='button-home' label="HTML" />
-        <Button variant='button-home' label="CSS" />
-        <Button variant='button-home' label="JAVASCRIPT" />
-        <Button variant='button-home'label="REACT" />
+        <Button
+          className="button-home"
+          label="HTML"
+          onClick={() => navigate("/quizz-html")}
+        />
+        <Button
+          className="button-home"
+          label="CSS"
+          onClick={() => navigate("/css")}
+        />
+        <Button
+          className="button-home"
+          label="JAVASCRIPT"
+          onClick={() => navigate("/javascript")}
+        />
+        <Button
+          className="button-home"
+          label="REACT"
+          onClick={() => navigate("/react")}
+        />
       </div>
     </div>
   );
