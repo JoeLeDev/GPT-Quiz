a22494773058e3254b47ba5103978af6cb5c8202
Jonathan Luembe <jonathanluembe@yahoo.com>
2024-02-23 08:28:55 +0100

    Detection chgt page

--- app/src/routes/HomePage/HomePage.jsx
+++ app/src/routes/HomePage/HomePage.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+
+  localStorage.setItem('hasLeftQuiz', 'true'); 
+
+  useEffect(() => {
+    function handleVisibilityChange() {
+      if (document.visibilityState === 'visible') {
+        console.log('Onglet est maintenant visible');
+        const hasLeft = localStorage.getItem('hasLeftQuiz');
+        if (hasLeft) {
+          console.log(`L'utilisateur avait quitté le quiz, redirection vers la page d\'erreur`);
+          navigate('/*');
+      }
+    }
+  }
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+
+    }, [navigate]);
+
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
